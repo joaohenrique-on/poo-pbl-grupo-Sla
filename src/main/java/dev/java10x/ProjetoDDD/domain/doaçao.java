6ba1ade72d070222e8ec76ee16f41cb66793e534
joaohenrique-on <joaohenriquepvs@gmail.com>
2026-06-07 17:47:57 -0300

    Alterações em doação

--- src/main/java/dev/java10x/ProjetoDDD/domain/doaçao.java
+++ src/main/java/dev/java10x/ProjetoDDD/domain/doaçao.java
@@ -1,6 +1,23 @@
 package dev.java10x.ProjetoDDD.domain;
 
-public class doaçao {
+import lombok.*;
+
+public class doacao {
+
+  private final @Getter usuario doador;
+  private final @Getter double valor;
+
+  public doacao(usuario doador, double valor){
+
+  if (valor <= 0) { 
+    throw new IllegalArgumentException("Valor deve ser maior que zero");
+  }
+    
+    this.doador = doador;
+    this.valor = valor;
+
+  }
+
 
     public boolean validar(double valor) {
         return valor > 0;
