f11e141165b3ee1d07b79d932b137bc37641b094
joaohenrique-on <joaohenriquepvs@gmail.com>
2026-06-07 17:39:52 -0300

    Tinha escrito errado

--- src/main/java/dev/java10x/ProjetoDDD/domain/usuario.java
+++ src/main/java/dev/java10x/ProjetoDDD/domain/usuario.java
@@ -2,12 +2,12 @@ package dev.java10x.ProjetoDDD.domain;
 
 import lombok.*;
 
-public class Usuario {
+public class usuario {
 
     private final @Getter String id;
     private @Getter @Setter String nome;
 
-    public Usuario(String id,String nome) {
+    public usuario(String id,String nome) {
 
         this.id = id;
         this.nome = nome;
